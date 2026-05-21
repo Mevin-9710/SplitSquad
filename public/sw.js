1c4f22a4e0dab9ff065a8ce30729a52f08c56483
Mevin <mevin@example.com>
2026-05-21 16:08:01 +0000

    fix: use S.png favicon from SpitsquadUI for browser tab logo

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ const STATIC_ASSETS = [
   '/app/css/tailwind.css',
   '/app/js/app.min.js',
   '/app/js/tutorial.min.js',
-  '/app/images/S.svg',
+  '/app/images/S.png',
   '/app/manifest.json',
 ];
 
